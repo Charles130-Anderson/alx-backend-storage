6051d251e3e64e19ba05757bced4866a606b6dba
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-11 12:40:40 +0000

    Add procedure to compute averages

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,24 @@
 -- Creates a stored procedure ComputeAverageScoreForUser
 -- that computes and stores the average score for a student
+
 DELIMITER $$
-
-CREATE PROCEDURE ComputeAverageScoreForUser(IN p_user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    -- Calculate the average score for the given user
-    UPDATE users
-    SET average_score = (
-        SELECT AVG(score) AS avg_score
-        FROM corrections
-        WHERE user_id = p_user_id
-    );
+    DECLARE total_score FLOAT;
+    DECLARE num_corrections INT;
+    
+    SELECT SUM(score), COUNT(*) INTO total_score, num_corrections
+    FROM corrections
+    WHERE user_id = ComputeAverageScoreForUser.user_id;
+    
+    IF num_corrections > 0 THEN
+        UPDATE users
+        SET average_score = total_score / num_corrections
+        WHERE id = ComputeAverageScoreForUser.user_id;
+    ELSE
+        UPDATE users
+        SET average_score = 0
+        WHERE id = ComputeAverageScoreForUser.user_id;
+    END IF;
 END$$
-
 DELIMITER ;
