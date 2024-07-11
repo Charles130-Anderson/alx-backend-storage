2e47309ecca6852109efdd0733d2995be0c2e4a9
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-11 11:38:33 +0000

    Create users table with ENUM.

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,3 +1,8 @@
+-- Create users table if not exists
+-- Define columns: id, email, name, country
+-- Set id as primary key
+-- Ensure email is unique
+-- Set default country to US
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
