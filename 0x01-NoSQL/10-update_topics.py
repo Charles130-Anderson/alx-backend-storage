834199669261561d78693adf5651c06d88a0c8be
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-16 14:11:11 +0000

    Update school topics in MongoDB collection.

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,7 +6,7 @@ Module to update topics of a school document in MongoDB collection
 
 def update_topics(mongo_collection, name, topics):
     """Updates topics of a school document based on its name"""
-    mongo_collection.update_one(
+    mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
