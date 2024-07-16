efce4f292ea43f3ec2be8b182204ebe31e3dec7c
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-16 18:02:46 +0000

    Added MongoDB log statistics script

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 
 HTTP_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
+
 def log_stats(collection, method_filter=None):
     """Analyze logs and print statistics."""
     total_logs = collection.count_documents({})
@@ -20,6 +21,7 @@ def log_stats(collection, method_filter=None):
 
     status_checks = collection.count_documents({"path": "/status"})
     print(f"{status_checks} status checks")
+
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
