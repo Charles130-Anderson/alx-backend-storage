0dce7924c124d5b63e1fda90dca6d6181017ec56
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-17 16:43:42 +0000

    Add get methods and conversions

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Cache module
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -29,3 +29,49 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieve data from Redis and optionally apply a conversion function.
+
+        Args:
+            key (str): The key of the data to retrieve.
+            fn (Optional[Callable]): A function to apply to the retrieved data.
+
+        Returns:
+            Union[str, bytes, int, float, None]:
+            The retrieved data after applying the
+            conversion function, if provided.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve data as a UTF-8 string.
+
+        Args:
+            key (str): The key of the data to retrieve.
+
+        Returns:
+            Optional[str]: The retrieved data as a UTF-8 string.
+        """
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve data as an integer.
+
+        Args:
+            key (str): The key of the data to retrieve.
+
+        Returns:
+            Optional[int]: The retrieved data as an integer.
+        """
+        return self.get(key, int)
